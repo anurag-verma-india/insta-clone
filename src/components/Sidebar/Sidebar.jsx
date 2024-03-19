92b9c687d852b21fd63128d6775b5dd16837428f
Anurag Verma <anuragsoniemail@gmail.com>
2024-03-19 13:58:31 +0530

    Added logo to Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,44 @@
+import { Box, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Box
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whiteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex direction={"column"} gap={10} w="full" height={"full"}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          cursor={"pointer"}
+        >
+          <InstagramLogo />
+        </Link>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          p={2}
+          display={{ base: "block", md: "none" }}
+          cursor={"pointer"}
+          borderRadius={6}
+          _hover={{bg: "whiteAlpha.200"}}
+          w={{base: 10}}
+        >
+          <InstagramMobileLogo />
+        </Link>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Sidebar;
